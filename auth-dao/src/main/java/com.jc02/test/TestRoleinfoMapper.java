c1fa2e9d36c3e17882e0d0008df7aed179e1b2bf
dengzhiyuyu <2773299016@qq.com>
2017-08-03 18:53:39 +0800

    mhvh

---
+++ auth-dao/src/main/java/com.jc02/test/TestRoleinfoMapper.java
@@ -0,0 +1,10 @@
+package com.jc02.test;/*
+ *  |　@author 邓智宇　　　　　　                                                                 　　　　　
+ *  |　@author 江西财经大学                                                   
+ *  |　@create 2017年08月03日 - 18:53　                    
+ *  |    @description   　　　　　　　　　　　　　                                                              　
+ */
+
+public class TestRoleinfoMapper {
+}
+
