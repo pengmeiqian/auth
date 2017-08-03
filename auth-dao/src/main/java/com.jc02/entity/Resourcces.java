7437208e2db25e4e4da0bac4b878e8e94f03ee66
pengmeiqian <1172648900@qq.com>
2017-08-03 16:53:09 +0800

    资源实体类

---
+++ auth-dao/src/main/java/com.jc02/entity/Resourcces.java
@@ -0,0 +1,107 @@
+package com.jc02.entity;
+
+import java.util.Date;
+
+/**
+ * 　　  　  　       \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　   　 (　 .@.@ 　)
+ * +-------oOOo-----( ~ )-----oOOo--------------------------------------+
+ * |　   @author 彭美倩
+ * |   　@author 江西财经大学
+ * |   　@create 2017年08月03日 - 16:46
+ * |    @description
+ * +---------------------------------Oooo-----------------------------+
+ */
+public class Resourcces {
+    private int rid;
+    private String rname;
+    private String resurl;
+    private int resorderno;
+    private String redsdes;
+    private Date createTime;
+    private Date updatetime;
+    private String createMan;
+    private String updateMan;
+    private int parentID;
+
+    public int getRid() {
+        return rid;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+
+    public String getResurl() {
+        return resurl;
+    }
+
+    public void setResurl(String resurl) {
+        this.resurl = resurl;
+    }
+
+    public int getResorderno() {
+        return resorderno;
+    }
+
+    public void setResorderno(int resorderno) {
+        this.resorderno = resorderno;
+    }
+
+    public String getRedsdes() {
+        return redsdes;
+    }
+
+    public void setRedsdes(String redsdes) {
+        this.redsdes = redsdes;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public String getCreateMan() {
+        return createMan;
+    }
+
+    public void setCreateMan(String createMan) {
+        this.createMan = createMan;
+    }
+
+    public String getUpdateMan() {
+        return updateMan;
+    }
+
+    public void setUpdateMan(String updateMan) {
+        this.updateMan = updateMan;
+    }
+
+    public int getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(int parentID) {
+        this.parentID = parentID;
+    }
+}
