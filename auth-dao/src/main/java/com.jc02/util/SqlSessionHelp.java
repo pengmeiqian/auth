09b85063efce18c418fa1f4088f6214f15d89605
dengzhiyuyu <2773299016@qq.com>
2017-08-03 19:04:05 +0800

    mhvh

--- auth-dao/src/main/java/com.jc02/util/SqlSessionHelp.java
+++ auth-dao/src/main/java/com.jc02/util/SqlSessionHelp.java
@@ -12,11 +12,11 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class SqlSessionHelp {
 	private static SqlSession session;
 	private static SqlSessionFactory factory;
-	private static Logger log = Logger.getLogger("SqlSessionHelp.class");
+
 	static{
 		Reader reader = null;
 		try {
-			reader = Resources.getResourceAsReader("mybatis.xml");
+			reader = Resources.getResourceAsReader("mybatis-config.xml");
 			System.out.println("读取配置文件成功");
 			factory=new SqlSessionFactoryBuilder().build(reader);
 			System.out.println("SqlSession工厂创建成功");
