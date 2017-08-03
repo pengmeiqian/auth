227f88adfa0f4c6ffc26d28887c23ee97308e5da
liuxueqin1234 <15279103361@163.com>
2017-08-03 18:42:02 +0800

    用户表

---
+++ auth-dao/src/main/java/com.jc02/test/TestUserinfo.java
@@ -0,0 +1,19 @@
+package com.jc02.test;/*
+ *  +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ *  |　@author chs-大傻逼　　　　　　                                                                 　|　　　　　
+ *  |　@author 17-7-20                                                  |
+ *  |　@create 2017年08月03日 - 18:36　                    
+ *  |    @description   测试用户表　　　　　　　　　　　　　                                                              　
+ *  +---------------------------------Oooo---------------------------------------+
+ */
+
+import com.jc02.util.SqlSessionHelp;
+import org.apache.ibatis.session.SqlSession;
+
+public class TestUserinfo {
+    public static void main(String[] args) {
+        SqlSession session=SqlSessionHelp.SqlSessionHelp();
+    }
+
+
+}
