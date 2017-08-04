4eed0c41ad10faa1a339549e21f46d6a12ad5d6b
liuxueqin1234 <15279103361@163.com>
2017-08-04 10:54:29 +0800

    servlet

---
+++ auth-web/src/main/java/com/jc/servlet/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.jc.servlet;/*
+ *  +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ *  |　@author chs-大傻逼　　　　　　                                                                 　|　　　　　
+ *  |　@author 17-7-20                                                  |
+ *  |　@create 2017年08月04日 - 10:46　                    
+ *  |    @description   登录界面　　　　　　　　　　　　　                                                              　
+ *  +---------------------------------Oooo---------------------------------------+
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
