707f8aec649677a8d5caf67b1a9a78b6bd12e278
liuxueqin1234 <15279103361@163.com>
2017-08-04 12:50:19 +0800

    servlet

--- auth-web/src/main/java/com/jc/servlet/LoginServlet.java
+++ auth-web/src/main/java/com/jc/servlet/LoginServlet.java
@@ -8,19 +8,21 @@ package com.jc.servlet;/*
  */
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet(name = "LoginServlet",urlPatterns = "/LoginServlet")
 public class LoginServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        super.doGet(req, resp);
     }
 }
